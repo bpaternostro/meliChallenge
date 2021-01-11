0394ac86194a2e02596a841020a14f4b749cc756
bpaternostro <bruno.paternostro@gmail.com>
2021-01-11 18:47:01 -0300

    Update TestHyper.py
    Modifique la invocación al método que había quedado mal.

--- test/TestHyper.py
+++ test/TestHyper.py
@@ -1,4 +1,4 @@
 from src.apiManager import Tableau as tableau
 #NOTA. No es una prueba unitaria. Es para validar el resultado.
 ta= tableau.Tableau()
-ta.readDataFromHyper("results")
+ta.read_data_from_hyper("results")
